7f89bbc7eef41efc4c232c34907620eb75731264
Errick Miron <highsman@lilaverse.app>
2024-05-25 16:49:08 -0700

    updated yaml and configure files.

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,21 +1,30 @@
-import Vapor
 import Fluent
 import FluentPostgresDriver
-//import Leaf
-// configure.swift
-
 import Vapor
 
 public func configure(_ app: Application) throws {
+    // Serve files from /Public folder
     app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
+    
+    app.http.server.configuration.port = 8011
+    
+    app.databases.use(.postgres(configuration: .init(
+        hostname: Environment.get("DATABASE_HOST") ?? "localhost",
+        port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? SQLPostgresConfiguration.ianaPortNumber,
+        username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
+        password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
+        database: Environment.get("DATABASE_NAME") ?? "vapor_database"
+    )), as: .psql)
 
-    // Configure the server to use HTTPS
-    app.http.server.configuration.hostname = "localhost"
-    app.http.server.configuration.port = 8011 // Change to an available port
-    app.http.server.configuration.tlsConfiguration = .makeServerConfiguration(
-        certificateChain: [.certificate(try .init(file: "/Users/errickwilliams/Desktop/AstrologicAPI/cert.pem", format: .pem))],
-        privateKey: .file("/Users/errickwilliams/Desktop/AstrologicAPI/key.pem")
+  //  app.migrations.add(CreateTodo())
+
+    let corsConfiguration = CORSMiddleware.Configuration(
+        allowedOrigin: .all,
+        allowedMethods: [.GET, .POST, .PUT, .OPTIONS, .DELETE, .PATCH],
+        allowedHeaders: [.accept, .authorization, .contentType, .origin, .xRequestedWith]
     )
+    let cors = CORSMiddleware(configuration: corsConfiguration)
+    app.middleware.use(cors)
 
     // Register routes
     try routes(app)
