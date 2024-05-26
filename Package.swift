62c2ff14c3460d5937c220bb2255ce2eec282888
Errick Miron <highsman@lilaverse.app>
2024-05-25 19:00:55 -0700

    Add Package.swift

---
+++ Package.swift
@@ -0,0 +1,48 @@
+// swift-tools-version:5.10
+import PackageDescription
+
+let package = Package(
+    name: "AstrologicAPI",
+    platforms: [
+       .macOS(.v13)
+    ],
+    dependencies: [
+        // 💧 A server-side Swift web framework.
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.99.3"),
+        // 🔵 Non-blocking, event-driven networking for Swift. Used for custom executors
+        .package(url: "https://github.com/apple/swift-nio.git", from: "2.65.0"),
+        // 🌌 SwiftEphemeris for astronomical calculations
+        .package(url: "https://github.com/heylila/SwiftEphemeris.git", .branch("my_new")),
+        // 🗃 Fluent ORM
+        .package(url: "https://github.com/vapor/fluent.git", from: "4.4.0"),
+        // 🟠 Fluent Postgres Driver
+        .package(url: "https://github.com/vapor/fluent-postgres-driver.git", from: "2.1.0")
+    ],
+    targets: [
+        .executableTarget(
+            name: "App",
+            dependencies: [
+                .product(name: "Vapor", package: "vapor"),
+                .product(name: "NIOCore", package: "swift-nio"),
+                .product(name: "NIOPosix", package: "swift-nio"),
+                .product(name: "SwiftEphemeris", package: "SwiftEphemeris"),
+                .product(name: "Fluent", package: "fluent"),
+                .product(name: "FluentPostgresDriver", package: "fluent-postgres-driver")
+            ],
+            swiftSettings: swiftSettings
+        ),
+        .testTarget(
+            name: "AppTests",
+            dependencies: [
+                .target(name: "App"),
+                .product(name: "XCTVapor", package: "vapor"),
+            ],
+            swiftSettings: swiftSettings
+        )
+    ]
+)
+
+var swiftSettings: [SwiftSetting] { [
+    .enableUpcomingFeature("DisableOutwardActorInference"),
+    .enableExperimentalFeature("StrictConcurrency"),
+] }
