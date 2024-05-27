621649907901f1966aa8a0506356eac68fb125b6
Errick Miron <highsman@lilaverse.app>
2024-05-27 00:56:35 -0700

    updated yaml

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,19 +1,21 @@
 import Vapor
-import Fluent
-import FluentPostgresDriver
+import NIOSSL
 
-public func configure(_ app: Application) throws {
-    // Database configuration, middlewares, migrations, etc.
-    app.databases.use(.postgres(configuration: .init(
-        hostname: Environment.get("DATABASE_HOST") ?? "localhost",
-        port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? SQLPostgresConfiguration.ianaPortNumber,
-        username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
-        password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
-        database: Environment.get("DATABASE_NAME") ?? "vapor_database",
-        tls: .prefer(try .init(configuration: .clientDefault)))
-    ), as: .psql)
-    
-    app.migrations.add(CreateTodo())
+public func configure(_ app: Application) async throws {
+    // Set up your database and other configurations here
+
+    // Configure SSL
+    let certPath = "/Users/errickwilliams/Desktop/AstrologicAPI/cert.pem"
+    let keyPath = "/Users/errickwilliams/Desktop/AstrologicAPI/key.pem"
+    let configuration = TLSConfiguration.makeServerConfiguration(
+        certificateChain: try NIOSSLCertificate.fromPEMFile(certPath).map { .certificate($0) },
+        privateKey: .file(keyPath)
+    )
+
+    app.http.server.configuration.hostname = "207.254.119.79"
+    app.http.server.configuration.port = 443
+    app.http.server.configuration.tlsConfiguration = configuration
+
     // Register routes
     try routes(app)
 }
