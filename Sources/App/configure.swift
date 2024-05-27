b79a3789b44610afca91bf06ec0c5b6e4349be3b
Errick Miron <highsman@lilaverse.app>
2024-05-27 15:12:20 -0700

    updated config

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -4,13 +4,27 @@ import NIOSSL
 public func configure(_ app: Application) async throws {
     // Set up your database and other configurations here
 
-    // Configure SSL
-    let certificatePath = "/Users/administrator/astrologicapi/cert.pem"
-    let privateKeyPath = "/Users/administrator/astrologicapi/key.pem"
-    let configuration = TLSConfiguration.makeServerConfiguration(
-        certificateChain: try NIOSSLCertificate.fromPEMFile(certificatePath).map { .certificate($0) },
-        privateKey: .file(privateKeyPath)
-    )
+    app.logger.info("Starting configuration")
+
+    let certPath = "/Users/administrator/astrologicapi/lilaastrology.com.crt"
+    let keyPath = "/Users/administrator/astrologicapi/lilaastrology.com.key"
+    //let caBundlePath = "/path/to/your/certificates/ca-bundle.crt"
+
+    app.logger.info("Using certPath: \(certPath)")
+    app.logger.info("Using keyPath: \(keyPath)")
+  //  app.logger.info("Using caBundlePath: \(caBundlePath)")
+
+    let configuration: TLSConfiguration
+    do {
+        configuration = TLSConfiguration.makeServerConfiguration(
+            certificateChain: try NIOSSLCertificate.fromPEMFile(certPath).map { .certificate($0) },
+            privateKey: .file(keyPath)
+         //   trustRoots: .file(caBundlePath)
+        )
+    } catch {
+        app.logger.critical("Failed to load SSL certificates: \(error)")
+        throw error
+    }
 
     app.http.server.configuration.hostname = "0.0.0.0"
     app.http.server.configuration.port = 443
